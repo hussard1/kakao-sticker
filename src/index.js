20955ed79b934ab22268d79fedef97f0739badc8
hussard <hussard>
2018-04-12 00:14:58 +0900

    init project

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const square = n => {
+    return n*n+1
+}
+
+document.querySelector("#test").innerHTML= "123123qw1qwe11asd123123e"
+
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+    entry: {
+        entry: './src/index.js'
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/dist/'
+    },
+    devServer: {
+        watchContentBase: true,
+        open: true,
+        port: 8000
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            include: [
+                path.resolve(__dirname, 'src')
+            ],
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['env']
+                }
+            }
+        }]
+    },
+    devtool: 'source-map',
+    mode: 'development'
+};
